5486b899d0a4d3496b8d5a5ed75bddf2f46c75af
UmutPatlak <umutpatlak77@gmail.com>
2025-06-05 01:01:12 +0300

    ilk commit

---
+++ KayitEkrani.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KutuphaneOtomasyonu
+{
+    public partial class KayitEkrani: Form
+    {
+        public KayitEkrani()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Basariyla Kaydedildi");
+        }
+    }
+}
